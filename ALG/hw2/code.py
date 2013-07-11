7104c5f249caad063ac59f9606d5f8c4e7cdcce7
Oleg <o.baranovsky@insollo.com>
2013-07-12 00:08:18 +0300

    hw2 w/o third part

--- ALG/hw2/code.py
+++ ALG/hw2/code.py
@@ -1,28 +1,35 @@
-PIVOT = 0
+from random import shuffle
+PIVOT = -1
 SWAPS = 0
 
-def quick_sort(arr):
-    print('inp', arr)
-    l = len(arr)
 
+def find_pivot(arr):
+    l = len(arr)//2
+    a = sorted([(arr[0], 0), (arr[l], l), (arr[-1], -1)])
+    return a[1][1]
+
+
+def quick_sort(arr):
+    global SWAPS
+    l = len(arr)
+    SWAPS += l - 1
     if l == 1:
         return arr
 
+    PIVOT = find_pivot(arr)
     arr[PIVOT], arr[0] = arr[0], arr[PIVOT]
     pivot = arr[0]
-    j = 1
-    for i in range(2, l):
+    j = 0
+    for i in range(1, l):
         if arr[i] < pivot:
-            arr[j], arr[i] = arr[i], arr[j]
+            arr[j+1], arr[i] = arr[i], arr[j+1]
             j += 1
 
-    j -= 1
     arr[0], arr[j] = arr[j], arr[0]
-    if j > 1:
+    if j > 0:
         arr[:j] = quick_sort(arr[:j])
-    if j < l-2:
+    if j < l-1:
         arr[j+1:] = quick_sort(arr[j+1:])
-    print('out', arr)
     return arr
 
 
@@ -30,10 +37,11 @@ def main():
     with open('QuickSort.txt', 'rt') as f:
         data = [int(r) for r in f.read().split('\n') if r]
 
-    arr = [5,2,3,2,6,7,8,32,32,3,2,1,3,4]
-    arr = [5,4,3,2]
-    print(arr)
-    print(quick_sort(arr))
+
+    find_pivot([2,4,23,5,3,220,3,45,13,455,22])
+
+    quick_sort(data)
+    print(SWAPS)
 
 
 if __name__ == '__main__':
