3bb468251bc40cbffa7bdecc4dd42bb9f01f13d0
Oleg <o.baranovsky@insollo.com>
2013-09-18 23:04:43 +0300

    added bits counting sort

--- ALG2/hw2/code.py
+++ ALG2/hw2/code.py
@@ -9,6 +9,51 @@ def hamming_distance(s1, s2):
     return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
 
 
+class Node:
+
+    def __init__(self, bits):
+        self.id = int(bits, 2)
+        self.ones = bits.count('1')
+        self.bits = bits
+        self.parent = None
+        self.rank = 0
+
+    def __hash__(self):
+        return self.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __lt__(self, other):
+        return self.ones < other.ones
+
+    def __gt__(self, other):
+        return self.ones > other.ones
+
+    def __repr__(self):
+        return '<Node:{} {} ones:{}>'.format(self.id, self.bits, self.ones)
+
+
+class UnionFind:
+
+    def __init__(self):
+        self.nodes = {}
+
+    def find(self, id):
+        if id not in seld.nodes:
+            return
+        parent = node = self.nodes[id]
+        while parent:
+            node = node.parent
+            parent = node.parent
+        return node
+
+    def union(self, id1, id2):
+        node1 = self.find(id1)
+        node2 = self.find(id2)
+
+
+
 def main():
 
     if sys.argv[-1] == '3':
@@ -23,20 +68,13 @@ def main():
                     continue
                 elif r == 'stop':
                     break
-                b = r.replace(' ', '')
-                i = int(b, 2)
-                lst.add((i, b))
+                lst.add(Node(r.replace(' ', '')))
 
-        clusters = 0
-        while lst:
-            c = lst.pop()
-            lst = sorted(lst, key=lambda x: hamming_distance(x[1], c[1]))
-            clusters += 1
-            print(clusters, len(lst))
-            for i, l in enumerate(lst):
-                if hamming_distance(l[1], c[1]) > 2:
-                    break
-            lst = lst[i:]
+        lst = sorted(lst)
+        c = lst[0]
+        for i, l in enumerate(lst):
+            if hamming_distance(l.bits, c.bits) < 3:
+                print('{:>10} {}'.format(i, l))
 
 
 if __name__ == '__main__':
